511b90d93e20ba196416b21bde5a4a4ac6ac400e
GianV-s <giancarlo.villachica@ucsp.edu.pe>
2026-05-21 19:32:15 -0500

    refactor: mejorar las PK de algunas tablas

--- 01_CREATE.sql
+++ 01_CREATE.sql
@@ -1,62 +1,61 @@
 CREATE TABLE PERSONA (
-  id_persona INTEGER PRIMARY KEY,
-  dni INTEGER NOT NULL UNIQUE,
-  nombre_cliente VARCHAR(100) NOT NULL UNIQUE,
-  fecha_nacimiento DATE NOT NULL,
-  telefono VARCHAR(12) NOT NULL UNIQUE
+  id_persona SERIAL PRIMARY KEY,
+  dni VARCHAR(8) NOT NULL UNIQUE,
+  nombre_cliente VARCHAR(100) NOT NULL UNIQUE,
+  fecha_nacimiento DATE NOT NULL,
+  telefono VARCHAR(12) NOT NULL UNIQUE
 );
 
 CREATE TABLE EMPRESA (
-  id_empresa INTEGER PRIMARY KEY,
-  nombre_cliente VARCHAR(100) NOT NULL UNIQUE,
-  categoria INTEGER NOT NULL,
-  ruc INTEGER NOT NULL UNIQUE,
-  iva REAL NOT NULL,
-  telefono VARCHAR(12) NOT NULL UNIQUE
+  id_empresa SERIAL PRIMARY KEY,
+  nombre_cliente VARCHAR(100) NOT NULL UNIQUE,
+  categoria INTEGER NOT NULL,
+  ruc VARCHAR(11) NOT NULL UNIQUE,
+  iva REAL NOT NULL,
+  telefono VARCHAR(12) NOT NULL UNIQUE
 );
 
 CREATE TABLE METODO_PAGO (
-  id_metodo INTEGER PRIMARY KEY,
-  tipo INTEGER NOT NULL
+  id_metodo VARCHAR(3) PRIMARY KEY,
+  tipo INTEGER NOT NULL
 );
 
 CREATE TABLE FACTURA (
-  id_factura INTEGER PRIMARY KEY,
-  monto INTEGER NOT NULL,
-  fecha_emision DATE NOT NULL,
-  id_empresa INTEGER, 
-  id_detalle INTEGER, 
-  id_metodo INTEGER,
-  FOREIGN KEY (id_empresa) REFERENCES EMPRESA(id_empresa),
-  FOREIGN KEY (id_detalle) REFERENCES DETALLE_PEDIDO(id_detalle),
-  FOREIGN KEY (id_metodo) REFERENCES METODO_PAGO(id_metodo)
+  id_factura SERIAL PRIMARY KEY,
+  monto INTEGER NOT NULL,
+  fecha_emision DATE NOT NULL,
+  id_empresa INTEGER, 
+  id_detalle INTEGER, 
+  id_metodo VARCHAR(3),
+  FOREIGN KEY (id_empresa) REFERENCES EMPRESA(id_empresa),
+  FOREIGN KEY (id_detalle) REFERENCES DETALLE_PEDIDO(id_detalle),
+  FOREIGN KEY (id_metodo) REFERENCES METODO_PAGO(id_metodo)
 );
 
 CREATE TABLE BOLETA (
-  id_boleta INTEGER PRIMARY KEY,
-  monto INTEGER NOT NULL,
-  fecha_emision DATE NOT NULL,
-  id_persona INTEGER, 
-  id_detalle INTEGER, 
-  id_metodo INTEGER,
-  FOREIGN KEY (id_persona) REFERENCES PERSONA(id_persona),
-  FOREIGN KEY (id_detalle) REFERENCES DETALLE_PEDIDO(id_detalle),
-  FOREIGN KEY (id_metodo) REFERENCES METODO_PAGO(id_metodo)
+  id_boleta SERIAL PRIMARY KEY,
+  monto INTEGER NOT NULL,
+  fecha_emision DATE NOT NULL,
+  id_persona INTEGER, 
+  id_detalle INTEGER, 
+  id_metodo VARCHAR(3),
+  FOREIGN KEY (id_persona) REFERENCES PERSONA(id_persona),
+  FOREIGN KEY (id_detalle) REFERENCES DETALLE_PEDIDO(id_detalle),
+  FOREIGN KEY (id_metodo) REFERENCES METODO_PAGO(id_metodo)
 );
 
 CREATE TABLE DIRECCION (
-  id_direccion INTEGER PRIMARY KEY,
-  pais VARCHAR(100) NOT NULL,
-  ciudad VARCHAR(100) NOT NULL,
-  distrito VARCHAR(100) NOT NULL,
-  calle VARCHAR(100) NOT NULL,
-  notas_adicionales VARCHAR(500) NOT NULL,
-  id_persona INTEGER,
-  id_empresa INTEGER,
-  FOREIGN KEY (id_persona) REFERENCES PERSONA(id_persona),
-  FOREIGN KEY (id_empresa) REFERENCES EMPRESA(id_empresa)
+  id_direccion SERIAL PRIMARY KEY,
+  pais VARCHAR(100) NOT NULL,
+  ciudad VARCHAR(100) NOT NULL,
+  distrito VARCHAR(100) NOT NULL,
+  calle VARCHAR(100) NOT NULL,
+  notas_adicionales VARCHAR(500) NOT NULL,
+  id_persona INTEGER,
+  id_empresa INTEGER,
+  FOREIGN KEY (id_persona) REFERENCES PERSONA(id_persona),
+  FOREIGN KEY (id_empresa) REFERENCES EMPRESA(id_empresa)
 );
-
 
 
 
