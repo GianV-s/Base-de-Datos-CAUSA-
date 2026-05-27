5544c980329b4183f75698bea4162f7abc297c32
victorgamarraucsp <victor.gamarra@ucsp.edu.pe>
2026-05-27 17:58:04 -0500

    update categoria
    modificar descripcion de una categoria

--- 03_UPDATE.sql
+++ 03_UPDATE.sql
@@ -6,3 +6,8 @@ WHERE dni = '11111111';
 UPDATE PRODUCTO
 SET precio = 49.90
 WHERE nombre = 'Mouse Logitech';
+
+-- 3 update
+UPDATE CATEGORIA
+SET descripcion = 'Productos electronicos y accesorios'
+WHERE nombre = 'Tecnologia';
