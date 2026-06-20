77ba8235b74466f5ce5070994110d860b7fed89c
gtvodk1 <gustavo.bustamante@ucsp.edu.pe>
2026-06-20 13:01:32 -0500

    Update 01_CREATE.sql

--- 01_CREATE.sql
+++ 01_CREATE.sql
@@ -20,46 +20,6 @@ CREATE TABLE METODO_PAGO (
  tipo INTEGER NOT NULL
 );
 
-CREATE TABLE FACTURA (
- id_factura SERIAL PRIMARY KEY,
- monto INTEGER NOT NULL,
- fecha_emision DATE NOT NULL,
- id_empresa INTEGER, 
- id_detalle INTEGER, 
- id_metodo VARCHAR(3),
- FOREIGN KEY (id_empresa) REFERENCES EMPRESA(id_empresa),
- FOREIGN KEY (id_detalle) REFERENCES DETALLE_PEDIDO(id_detalle),
- FOREIGN KEY (id_metodo) REFERENCES METODO_PAGO(id_metodo)
-);
-
-CREATE TABLE BOLETA (
- id_boleta SERIAL PRIMARY KEY,
- monto INTEGER NOT NULL,
- fecha_emision DATE NOT NULL,
- id_persona INTEGER, 
- id_detalle INTEGER, 
- id_metodo VARCHAR(3),
- FOREIGN KEY (id_persona) REFERENCES PERSONA(id_persona),
- FOREIGN KEY (id_detalle) REFERENCES DETALLE_PEDIDO(id_detalle),
- FOREIGN KEY (id_metodo) REFERENCES METODO_PAGO(id_metodo)
-);
-
-CREATE TABLE DIRECCION (
- id_direccion SERIAL PRIMARY KEY,
- pais VARCHAR(100) NOT NULL,
- ciudad VARCHAR(100) NOT NULL,
- distrito VARCHAR(100) NOT NULL,
- calle VARCHAR(100) NOT NULL,
- notas_adicionales VARCHAR(500) NOT NULL,
- id_persona INTEGER,
- id_empresa INTEGER,
- FOREIGN KEY (id_persona) REFERENCES PERSONA(id_persona),
- FOREIGN KEY (id_empresa) REFERENCES EMPRESA(id_empresa)
-);
-
-
-
-
 CREATE TABLE CATEGORIA (
   id_categoria INTEGER PRIMARY KEY,
   nombre VARCHAR(100) NOT NULL,
@@ -76,12 +36,75 @@ CREATE TABLE PRODUCTO (
   FOREIGN KEY (id_categoria) REFERENCES CATEGORIA(id_categoria)
 );
 
-
 CREATE TABLE DESCUENTO (
   id_descuento INTEGER PRIMARY KEY,
   descuento REAL NOT NULL,
   fecha_limite DATE NOT NULL,
   estado VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE ESTADO_ENVIO (
+  id_estado INTEGER PRIMARY KEY,
+  repartidor VARCHAR(100) NOT NULL,
+  estado VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE ALMACEN (
+    id_almacen INTEGER PRIMARY KEY,
+    nombre_almacen VARCHAR(100) NOT NULL,
+    direccion_fisica VARCHAR(200) NOT NULL,
+    capacidad_total INTEGER NOT NULL,
+    tipo_almacen VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE STOCK ( 
+    id_stock INTEGER PRIMARY KEY,
+    cantidad_disponible INTEGER NOT NULL,
+    maximo INTEGER NOT NULL,
+    id_producto INTEGER,
+    id_almacen INTEGER,
+    FOREIGN KEY (id_producto) REFERENCES PRODUCTO(id_producto),
+    FOREIGN KEY (id_almacen) REFERENCES ALMACEN(id_almacen)
+);
+
+CREATE TABLE CLIENTE ( 
+    id_cliente SERIAL PRIMARY KEY,
+    telefono_contacto VARCHAR(20) NOT NULL,
+    id_persona INTEGER UNIQUE,
+    id_empresa INTEGER UNIQUE,
+    FOREIGN KEY (id_persona) REFERENCES PERSONA(id_persona),
+    FOREIGN KEY (id_empresa) REFERENCES EMPRESA(id_empresa)
+);
+
+CREATE TABLE RESEÑA ( 
+    id_reseña INTEGER PRIMARY KEY,
+    puntuacion INTEGER NOT NULL,
+    descripcion VARCHAR(300) NOT NULL,
+    id_producto INTEGER,
+    id_cliente INTEGER,
+    FOREIGN KEY (id_producto) REFERENCES PRODUCTO(id_producto),
+    FOREIGN KEY (id_cliente) REFERENCES CLIENTE(id_cliente)
+);
+
+CREATE TABLE PEDIDO ( 
+    id_pedido SERIAL PRIMARY KEY,
+    fecha_pedido DATE NOT NULL,
+    fecha_entrega_est DATE NOT NULL,
+    cantidad INTEGER NOT NULL,
+    id_cliente INTEGER NOT NULL,
+    id_metodo VARCHAR(3) NOT NULL,
+    id_estado_envio INTEGER,
+    FOREIGN KEY (id_cliente) REFERENCES CLIENTE(id_cliente),
+    FOREIGN KEY (id_metodo) REFERENCES METODO_PAGO(id_metodo),
+    FOREIGN KEY (id_estado_envio) REFERENCES ESTADO_ENVIO(id_estado)
+);
+
+CREATE TABLE COMPROBANTE ( 
+    id_comprobante SERIAL PRIMARY KEY,
+    monto REAL NOT NULL,
+    fecha_emision DATE NOT NULL,
+    id_pedido INTEGER UNIQUE,
+    FOREIGN KEY (id_pedido) REFERENCES PEDIDO(id_pedido)
 );
 
 CREATE TABLE DETALLE_PEDIDO (
@@ -92,24 +115,58 @@ CREATE TABLE DETALLE_PEDIDO (
   total REAL NOT NULL,
   id_descuento INTEGER,
   id_producto INTEGER,
+  id_pedido INTEGER, 
   FOREIGN KEY (id_descuento) REFERENCES DESCUENTO(id_descuento),
-  FOREIGN KEY (id_producto) REFERENCES PRODUCTO(id_producto)
+  FOREIGN KEY (id_producto) REFERENCES PRODUCTO(id_producto),
+  FOREIGN KEY (id_pedido) REFERENCES PEDIDO(id_pedido)
 );
-
 
 CREATE TABLE DEVOLUCION (
   id_devolucion INTEGER PRIMARY KEY,
   estado VARCHAR(50) NOT NULL,
   fecha_registro DATE NOT NULL,
-  descripcion VARCHAR(300) NOT NULL
+  descripcion VARCHAR(300) NOT NULL,
+  id_pedido INTEGER, 
+  FOREIGN KEY (id_pedido) REFERENCES PEDIDO(id_pedido) 
 );
 
-
-CREATE TABLE ESTADO_ENVIO (
-  id_estado INTEGER PRIMARY KEY,
-  repartidor VARCHAR(100) NOT NULL,
-  estado VARCHAR(50) NOT NULL
+CREATE TABLE FACTURA (
+ id_factura SERIAL PRIMARY KEY,
+ monto INTEGER NOT NULL,
+ fecha_emision DATE NOT NULL,
+ id_empresa INTEGER, 
+ id_detalle INTEGER, 
+ id_metodo VARCHAR(3),
+ id_comprobante INTEGER, 
+ FOREIGN KEY (id_empresa) REFERENCES EMPRESA(id_empresa),
+ FOREIGN KEY (id_detalle) REFERENCES DETALLE_PEDIDO(id_detalle),
+ FOREIGN KEY (id_metodo) REFERENCES METODO_PAGO(id_metodo),
+ FOREIGN KEY (id_comprobante) REFERENCES COMPROBANTE(id_comprobante) 
 );
 
+CREATE TABLE BOLETA (
+ id_boleta SERIAL PRIMARY KEY,
+ monto INTEGER NOT NULL,
+ fecha_emision DATE NOT NULL,
+ id_persona INTEGER, 
+ id_detalle INTEGER, 
+ id_metodo VARCHAR(3),
+ id_comprobante INTEGER,
+ FOREIGN KEY (id_persona) REFERENCES PERSONA(id_persona),
+ FOREIGN KEY (id_detalle) REFERENCES DETALLE_PEDIDO(id_detalle),
+ FOREIGN KEY (id_metodo) REFERENCES METODO_PAGO(id_metodo),
+ FOREIGN KEY (id_comprobante) REFERENCES COMPROBANTE(id_comprobante) 
+);
 
-
+CREATE TABLE DIRECCION (
+ id_direccion SERIAL PRIMARY KEY,
+ pais VARCHAR(100) NOT NULL,
+ ciudad VARCHAR(100) NOT NULL,
+ distrito VARCHAR(100) NOT NULL,
+ calle VARCHAR(100) NOT NULL,
+ notas_adicionales VARCHAR(500) NOT NULL,
+ id_persona INTEGER,
+ id_empresa INTEGER,
+ FOREIGN KEY (id_persona) REFERENCES PERSONA(id_persona),
+ FOREIGN KEY (id_empresa) REFERENCES EMPRESA(id_empresa)
+);
